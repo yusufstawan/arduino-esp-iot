f9528990b19d54b8ce537c06b19a2ef028176456
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-02 15:31:17 +0700

    esp32 potensio

--- ESP32/Sensor/Potensio/Potensio.ino
+++ ESP32/Sensor/Potensio/Potensio.ino
@@ -3,31 +3,42 @@
  * Sensor : Potensio
  * Board  : ESP32
  * Output : Serial (9600)
- * Analog :
- * Recommended pin use: A0
+ * Analog : D4, D2, D15, D13, D12, D14, D27, D26, D25, D33, D32, D35, D34, VN, VP
+ * Recommended pin use: D2 (GPIO 2)
  *********************************/
 
 // pilih salah satu pin untuk koneksi sensor
 int potensioPin = 2;
+// int potensioPin = 4;
+// int potensioPin = 15;
+// int potensioPin = 13;
+// int potensioPin = 12;
+// int potensioPin = 14;
+// int potensioPin = 27;
+// int potensioPin = 26;
+// int potensioPin = 25;
+// int potensioPin = 33;
+// int potensioPin = 32;
+// int potensioPin = 35;
+// int potensioPin = 34;
+// int potensioPin = 39;
+// int potensioPin = 36;
 
 void setup()
 {
-  // initialize serial communication at 115200 bits per second:
-  Serial.begin(115200);
-
-  // set the resolution to 12 bits (0-4096)
+  Serial.begin(9600);
+  // buat resolusi 12 bit (0-4096)
   analogReadResolution(12);
 }
 
 void loop()
 {
-  // read the analog / millivolts value for pin 2:
+  // baca potensio value
   int analogValue = analogRead(potensioPin);
   int analogVolts = analogReadMilliVolts(potensioPin);
 
-  // print out the values you read:
+  // tampilkan ke serial monitor
   Serial.printf("ADC analog value = %d\n", analogValue);
   Serial.printf("ADC millivolts value = %d\n", analogVolts);
-
-  delay(100); // delay in between reads for clear read from serial
+  delay(1000);
 }
