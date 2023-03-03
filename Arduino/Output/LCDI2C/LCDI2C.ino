252389e033968e2e0315726f7661351ba546cb33
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-03 10:06:35 +0700

    arduino lcd

--- Arduino/Output/LCDI2C/LCDI2C.ino
+++ Arduino/Output/LCDI2C/LCDI2C.ino
@@ -1,11 +1,11 @@
-/**************************************
- *  Modul I2C LCD dengan Arduino UNO
- *  Koneksi modul I2C LCD dengan Arduino :
- *  Vcc  -- 5V
- *  Gnd  -- GND
- *  SDA  -- pin A4
- *  SCL  -- pin A5
- *  *********************************/
+/*********************************
+ * Arduino Uno I2C LCD
+ * Board  : Arduino Uno
+ * Output : LCD 16x2
+ * SDA    : A4
+ * SCL    : A5
+ *********************************/
+
 #include <Wire.h>              // Library komunikasi I2C
 #include <LiquidCrystal_I2C.h> // Library modul I2C LCD
 
@@ -15,15 +15,29 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 void setup()
 {
-  // inisialisasi LCD:
   lcd.init();
-  lcd.backlight();
   lcd.clear();
-  lcd.print("Modul I2C LCD");
-  lcd.setCursor(0, 1);
-  lcd.print("www.ardutech.com");
+  lcd.backlight();
+
+  lcd.setCursor(2, 0); // Set cursor to character 2 on line 0
+  lcd.print("Hello World!");
+
+  lcd.setCursor(2, 1); // Move cursor to character 2 on line 1
+  lcd.print("Use I2C LCD");
 }
 
 void loop()
 {
+  for (int i = 0; i < 16; i++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(500);
+  }
+  delay(1000);
+  for (int i = 0; i < 16; i++)
+  {
+    lcd.scrollDisplayRight();
+    delay(500);
+  }
+  delay(1000);
 }
