080039b636d40b4be820d2ca1287c49ecdbb99e3
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-08 10:25:33 +0700

    arduino rfid-rc522

--- Arduino/Sensor/Rfid/Rfid.ino
+++ Arduino/Sensor/Rfid/Rfid.ino
@@ -1,3 +1,10 @@
+/*********************************
+ * Membaca Tag RFID
+ * Sensor : RFID-RC522
+ * Board  : Arduino UNO
+ * Output : Serial (9600)
+ *********************************/
+
 byte sda = 10;
 byte rst = 9;
 #include <KRrfid.h>
@@ -16,5 +23,6 @@ void loop()
     Serial.print("TAG :");
     Serial.println(TAG);
     TAG = "";
+    delay(500);
   }
 }
---
+++ ESP32/Sensor/Rfid/Rfid.ino
@@ -0,0 +1,6 @@
+/*********************************
+ * Membaca Tag RFID
+ * Sensor : RFID-RC522
+ * Board  : ESP32
+ * Output : Serial (9600)
+ *********************************/
