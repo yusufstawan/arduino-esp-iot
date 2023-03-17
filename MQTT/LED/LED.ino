736ce1e733c224c1d026b0629ee06d4101e5e765
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-17 08:32:18 +0700

    init servo & led mqtt

---
+++ MQTT/LED/LED.ino
@@ -0,0 +1,19 @@
+#include "env.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <LiquidCrystal_I2C.h>
+
+// WiFi
+const char *ssid = wifi_ssid;
+const char *password = wifi_password;
+
+// MQTT Broker
+const char *mqtt_broker = mqtt_broker_server;
+const int mqtt_port = mqtt_port_server;
+const char *mqtt_username = mqtt_username_server;
+const char *mqtt_password = mqtt_password_server;
+const char *topic = "/VARX/dht11";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
